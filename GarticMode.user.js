14253f88389d90e1e6bd02778a697845fd0bf833
69Type <72820911+69Type@users.noreply.github.com>
2021-05-17 16:59:34 +0300

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -1,10 +1,5 @@
-(function(that){((context, fapply, console) => {with (context) {(module => {"use strict";try {fapply(module, context, [,,context.CDATA,context.uneval,context.define,context.module,context.exports,context.GM,context.GM_info]);} catch (e) {if (e.message && e.stack) {console.error("ERROR: Execution of script 'GarticMode' failed! " + e.message);console.log(e.stack.replace(/(\\(eval at )?<anonymous>[: ]?)|([\s.]*at Object.tms_[\s\S.]*)/g, ""));} else {console.error(e);}}})(function (context,fapply,CDATA,uneval,define,module,exports,GM,GM_info) {
-
-//var cv = document.opencv;
-//console.log(cv)
 
 //////////////////////////////////////////////////////style///////////////////////////////////////////////
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 var cssAnimation = document.createElement('style');
 cssAnimation.type = 'text/css';
@@ -3574,6 +3569,5 @@ function exec() {
 
 alert("FUCK");
 exec();
-})}})(that.context, that.fapply, that.console);
-               })((()=>{const k="__u__14101777.66766793",r=this[k];delete this[k];return r;})())
+
 
