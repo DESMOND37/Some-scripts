cdbfd69c399f6fcf5bf0cfcae9744cbf764f38d5
69Type <72820911+69Type@users.noreply.github.com>
2021-05-19 02:22:56 +0300

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -1,5 +1,5 @@
 
-var VERSION = "2.2.01";
+var VERSION = "2.2.0";
 
 if (getCookieDict().VERSION != VERSION){
     alert(`НОВАЯ ВЕРСИЯ! ${VERSION}\nНовости новой версии:\nТеперь скрывать холст можно и в разделе memory`);
